f3ceca7e4ab8f4ae171f2c1d2f8519381a947698
chominsoo4245 <chominsoo4245@gmail.com>
2023-02-26 19:10:14 +0900

    first commit

--- src/main/java/com/pass/PassWebApplication.java
+++ src/main/java/com/pass/PassWebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PassWebApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PassWebApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PassWebApplication.class, args);
+	}
 
 }
--- src/test/java/com/pass/PassWebApplicationTests.java
+++ src/test/java/com/pass/PassWebApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PassWebApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
